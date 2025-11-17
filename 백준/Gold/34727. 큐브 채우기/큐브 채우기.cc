c4af0d18e7c1f4dd4aa49730ffbcce5dfc35d4df
Junha Hwang <84448319+junnha@users.noreply.github.com>
2025-11-17 13:44:52 +0900

    [Gold II] Title: 큐브 채우기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/34727. 큐브 채우기/큐브 채우기.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        cin >> N;
+        cout << (N % 2 == 1 ? 1 : 0) << "\n";
+    }
+}

60ef62affc3804e64d7ee8ee844c08f95fc5e62d
Junha Hwang <84448319+junnha@users.noreply.github.com>
2025-11-17 14:08:23 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1;
+    int num2;
+    int num3;
+    
+    scanf("%d %d\n" , &num1, &num2);
+    
+    num3 = num1 + num2;
+    
+    printf("%d\n", num3);
+    
+    return 0;
+}

c70cb26a6f1027ce4022e607260a3c735789cea0
Junha Hwang <84448319+junnha@users.noreply.github.com>
2025-12-31 17:44:44 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vl vector<ll>
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define minpq(type) priority_queue<type, vector<type>, greater<type>>
+#define maxpq(type) priority_queue<type>
+
+#define fastIO ios::sync_with_stdio(false); cin.tie(nullptr)
+
+void solve() {
+    int N;
+    cin >> N;
+    int cnt = N / 4;
+    while(cnt--) {
+        cout << "long" << " ";
+    }
+    cout << "int";
+}
+
+int main() {
+    fastIO;
+    solve();
+}

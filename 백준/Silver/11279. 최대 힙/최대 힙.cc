8b1dc0dd6d47fae4e2912e0c4da4c40bcc3282d8
Junha Hwang <84448319+junnha@users.noreply.github.com>
2025-12-31 18:09:44 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 3684 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vl vector<ll>
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define minpq(type) priority_queue<type, vector<type>, greater<type>>
+#define maxpq(type) priority_queue<type>
+
+#define fastIO ios::sync_with_stdio(false); cin.tie(nullptr)
+
+void solve() {
+    ll N;
+    priority_queue<ll> pq;
+    cin >> N;
+
+    while(N--) {
+        ll k;
+        cin >> k;
+
+        if(k==0) {
+            if(!pq.empty()) {
+                cout << pq.top() << '\n';
+                pq.pop();
+            } else cout << 0 << '\n';
+        } else pq.push(k);
+    }
+}
+
+int main() {
+    fastIO;
+    solve();
+}

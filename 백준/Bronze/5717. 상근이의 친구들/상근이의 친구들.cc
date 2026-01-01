8ecd028f8582290b6d33f2889b1488b3d279e298
Junha Hwang <84448319+junnha@users.noreply.github.com>
2026-01-01 16:25:07 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vl vector<ll>
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define minpq(type) priority_queue<type, vector<type>, greater<type>>
+#define maxpq(type) priority_queue<type>
+
+#define fastIO ios::sync_with_stdio(false); cin.tie(nullptr)
+
+void solve() {
+    int a,b;
+    while(1) {
+        cin >> a >> b;
+        if(a==0&&b==0) break;
+        int ans = a+b;
+        cout << ans << '\n';
+    }
+}
+
+int main() {
+    fastIO;
+    solve();
+}

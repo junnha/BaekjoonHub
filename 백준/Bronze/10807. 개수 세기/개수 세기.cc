e48a68054d13c9a2017ac4277260e01ba1530318
Junha Hwang <84448319+junnha@users.noreply.github.com>
2026-01-07 13:40:52 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vl vector<ll>
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define minpq(type) priority_queue<type, vector<type>, greater<type>>
+#define maxpq(type) priority_queue<type>
+
+#define fastIO ios::sync_with_stdio(false); cin.tie(nullptr)
+
+void solve() {
+    int N;
+    cin >> N;
+    int arr[102];
+    for(int i=0;i<N;i++) {
+        cin >> arr[i];
+    }
+    int K;
+    cin >> K;
+    int cnt = 0;
+    for(int j=0;j<N;j++) {
+        if(arr[j] == K) {
+            cnt++;
+        }
+    }
+    cout << cnt;
+}
+
+int main() {
+    fastIO;
+    solve();
+}

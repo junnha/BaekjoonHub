543199f7e3bd212ad6e997708b5c4d9aebd17166
Junha Hwang <84448319+junnha@users.noreply.github.com>
2026-01-10 16:36:50 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 240 ms, Memory: 8292 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vl vector<ll>
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define minpq(type) priority_queue<type, vector<type>, greater<type>>
+#define maxpq(type) priority_queue<type>
+
+#define fastIO ios::sync_with_stdio(false); cin.tie(nullptr)
+
+void solve() {
+    int N;
+    cin >> N;
+    vi arr;
+    for(int i=0;i<N;i++) {
+        int k;
+        cin >> k;
+        arr.push_back(k);
+    }
+    sort(arr.begin(),arr.end(),greater<>());
+    for(int i=0;i<N;i++) {
+        cout << arr[i] << '\n';
+    }
+}
+
+int main() {
+    fastIO;
+    solve();
+}

147925cde5743a32fc9b3c0b34419abb5813711d
Junha Hwang <84448319+junnha@users.noreply.github.com>
2026-01-13 17:11:20 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vl vector<ll>
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define minpq(type) priority_queue<type, vector<type>, greater<type>>
+#define maxpq(type) priority_queue<type>
+
+#define fastIO ios::sync_with_stdio(false); cin.tie(nullptr)
+
+int N;
+
+void solve() {
+    cin >> N;
+    int ans = N * (N-1);
+    cout << ans;
+}
+
+int main() {
+    fastIO;
+    solve();
+}

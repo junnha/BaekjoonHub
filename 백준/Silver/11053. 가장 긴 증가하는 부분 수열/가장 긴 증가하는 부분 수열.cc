eaea98fa1d39d480a210a0d35b079c5f455f03cf
Junha Hwang <84448319+junnha@users.noreply.github.com>
2026-01-20 14:07:19 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vl vector<ll>
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define minpq(type) priority_queue<type, vector<type>, greater<type>>
+#define maxpq(type) priority_queue<type>
+
+#define fastIO ios::sync_with_stdio(false); cin.tie(nullptr)
+
+int N;
+int arr[1005];
+int dp[1005];
+
+void solve() {
+    cin >> N;
+    for(int i=0;i<N;i++) {
+        cin >> arr[i];
+    }
+    int ans = 0;
+    for(int i=0;i<N;i++) {
+        dp[i]=1;
+        for(int j=0;j<i;j++) {
+            if(arr[j]<arr[i]) {
+                dp[i] = max(dp[i], dp[j]+1);
+            }
+        }
+        ans = max(ans, dp[i]);
+    }
+    cout << ans;
+}
+
+int main() {
+    fastIO;
+    solve();
+}

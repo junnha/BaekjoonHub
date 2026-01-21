748d684c210400ff9792b171aadaa1591d3b5325
Junha Hwang <84448319+junnha@users.noreply.github.com>
2026-01-21 16:26:54 +0900

    [Bronze I] Title: 소인수분해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vl vector<ll>
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define minpq(type) priority_queue<type, vector<type>, greater<type>>
+#define maxpq(type) priority_queue<type>
+
+#define fastIO ios::sync_with_stdio(false); cin.tie(nullptr)
+
+int N;
+
+void solve() {
+    cin >> N;
+    for(int i=2;i*i<=N;i++) {
+        while(N%i==0) {
+            cout << i << '\n';
+            N /= i;
+        }
+    }
+    if(N!=1) cout << N;
+}
+
+int main() {
+    fastIO;
+    solve();
+}

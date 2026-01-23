1b0e72cd1d0e20a2778b2512218e1d347bbb6781
Junha Hwang <84448319+junnha@users.noreply.github.com>
2026-01-23 15:34:53 +0900

    [Silver II] Title: 좌표 압축, Time: 448 ms, Memory: 18348 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vl vector<ll>
+
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define minpq(type) priority_queue<type, vector<type>, greater<type>>
+#define maxpq(type) priority_queue<type>
+
+#define fastIO ios::sync_with_stdio(false); cin.tie(nullptr)
+
+int N;
+int arr[1000005];
+vector<int> tmp, uni;
+int main(void){
+    fastIO;
+    cin >> N;
+    for(int i=0;i<N;i++){
+        cin >> arr[i];
+        tmp.push_back(arr[i]);
+    }
+    sort(tmp.begin(), tmp.end());
+    
+    for(int i=0;i<N;i++){
+        if(i==0 || tmp[i-1]!=tmp[i])
+            uni.push_back(tmp[i]);
+        }
+    for(int i=0;i<N;i++)
+    cout << lower_bound(uni.begin(),uni.end(),arr[i])-uni.begin() << ' ';
+}

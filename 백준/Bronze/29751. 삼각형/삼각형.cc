782f7989bd9500564b192570be454b9112f69b03
Junha Hwang <84448319+junnha@users.noreply.github.com>
2026-02-23 13:49:40 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    cin >> a >> b;
+    double area = (double)a * b / 2.0;
+    cout << fixed << setprecision(1) << area;
+}

43a75becf5098eeb09529a8fc6ca10d644b7e0b5
Junha Hwang <84448319+junnha@users.noreply.github.com>
2026-03-03 09:55:10 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n,m;
+    int k;
+    cin >> k;
+    for(int i=0;i<k;i++) {
+        cin >> n >> m;
+        cout << "yes" << '\n';
+    }
+}
